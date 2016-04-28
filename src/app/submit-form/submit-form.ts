4f77e5b29e9d6187e7c252907c98c90ebdb51638
TAbdiukov <TAbdiukov@gmail.com>
2016-04-28 18:26:36 +0800

    Update submit-form.ts

--- src/app/submit-form/submit-form.ts
+++ src/app/submit-form/submit-form.ts
@@ -25,12 +25,6 @@
             fbutil.ref('forms').push({
                 'clientId': user.uid,
                 'name': vm.generalInformation.name
-                'dateOfBirth': vm.generalInformation.dateOfBirth
-                'placeOfBirth': vm.generalInformation.placeOfBirth
-                'countryOfBirth': vm.generalInformation.countryOfBirth
-                'daytimeTelephoneNumber': vm.generalInformation.daytimeTelephoneNumber
-                'mobileTelephoneNumber': vm.generalInformation.mobileTelephoneNumber
-                'email': vm.generalInformation.email
              }, function(error) {
                 if (error) {
                     logger.error('Form submit failed', error, 'Error');
