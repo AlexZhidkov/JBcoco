85273a56b11c6c5aa7c1f5910f2635a92ad3d22e
TAbdiukov <TAbdiukov@gmail.com>
2016-05-01 14:35:39 +0800

    Minor improvements
    * Added avatars to the candidates list
    * "ClientId" was replaced with "Date Submitted"
    * "IdentificationDetails" adding page attempt
    * Fixed "deleteItem" button

--- src/app/submit-form/submit-form.ts
+++ src/app/submit-form/submit-form.ts
@@ -22,9 +22,29 @@
         }
         
         function submit() {
+            var today = new Date();
+            var dd = today.getDate();
+            var mm = today.getMonth()+1; //January is 0!
+            var yyyy = today.getFullYear();
+            if(dd<10) {
+	        dd='0'+dd
+            } 
+
+            if(mm<10) {
+                mm='0'+mm
+            }
+             
             fbutil.ref('forms').push({
                 'clientId': user.uid,
-                'name': vm.generalInformation.name
+                'name': vm.generalInformation.name,
+                'dateOfBirth': vm.generalInformation.dateOfBirth,
+                'placeOfBirth': vm.generalInformation.placeOfBirth,
+                'countryOfBirth': vm.generalInformation.countryOfBirth,
+                'daytimeTelephoneNumber': vm.generalInformation.daytimeTelephoneNumber,
+                'mobileTelephoneNumber': vm.generalInformation.mobileTelephoneNumber,
+                'email': vm.generalInformation.email
+                'dateSubmitted': mm+'/'+dd+'/'+yyyy;
+                  
              }, function(error) {
                 if (error) {
                     logger.error('Form submit failed', error, 'Error');
